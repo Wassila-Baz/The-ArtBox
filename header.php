77e812919b70f2220aa6f3127f31a75818650863
Wassila-Baz <wassila.baz@icloud.com>
2024-06-18 15:09:47 +0200

    maj

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <header>
-        <a href="index.html"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
+        <a href="./index.php"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
         <nav>
             <ul>
                 <li><a href="index.php">Accueil</a></li>
